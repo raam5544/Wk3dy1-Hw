168f3eab4483a4ecf552d0b991b5965905550dce
raam <jeya.colombo@gmail.com>
2023-10-18 18:16:59 -0400

    Final attempt

--- wk3dy1hw.js
+++ wk3dy1hw.js
@@ -1,99 +1,93 @@
 // Question 02
 
-function maxOfThree(...nums) {
-    let max = 0;
+const maxOfThree = function (...nums) {
+    let max = nums[0];
     for (let num of nums) {
-        if (typeof num !== 'number') return console.log(('Please enter valid arguements'))
-        if (num > max) max = num
+        if (typeof num !== 'number') return console.log(('Please enter valid arguements'));
+        if (num > max) max = num;
     }
-    console.log(max)
+    console.log(max);
 }
-maxOfThree(-93, -67, 3)
+
+// maxOfThree(-93, -67, -3);
+
 
 // Question 03
 
 function isCharVowel(char) {
-    const vowels = ['a', 'e', 'i', 'o', 'u']
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
     let decision = false;
     for (let vowel of vowels) {
         if (char === vowel.toUpperCase() || char === vowel.toLowerCase()) {
-            decision = true
+            decision = true;
         }
     }
-    console.log(decision)
+    console.log(decision);
 }
 
-// isCharVowel('s')
+// isCharVowel('B');
 
 //Question 04
 
-function sumArray(...nums) {
-    let sum = 0
+const sumArray = function (nums) {
+    let sum = 0;
     for (let num of nums) {
-        sum += num
+        sum += num;
     }
-    console.log(sum)
+    console.log(sum);
 }
 
-// sumArray(2, 3, 4, 6, 5)
+// sumArray([2, 4, 5]);
 
 // Question 05
 
 function multiplyArray(...nums) {
-    answ = 1
+    answ = 1;
     for (let num of nums) {
-        answ *= num
+        answ *= num;
     }
-    console.log(answ)
+    console.log(answ);
 }
-// multiplyArray(2, 3, 4)
+// multiplyArray(2, 3, 4);
 
 // Question 06
 
-function numArgs(...arg) {
-    console.log(arg.length)
+const numArgs = function (...arg) {
+    console.log(arg.length);
 }
 
-// numArgs('r', 7, [4, 5, 4])
+// numArgs([4, 5, 4], 5, 'r');
 
-// Question 06
-
-// function reverseString(str) {
-//     let reverseStringArray = []
-//     let i = -1
-//     while (i >= -(str.length)) {
-//         reverseStringArray.un(str.slice(i))
-//         i--
-//     }
-//     console.log(reverseStringArray)
-// }
-
-// reverseString('rockstar')
+// Question 07
 
 function reverseString(str) {
-    let reverseStringArray = []
+    let reverseStringArray = [];
     for (let char of str) {
-        reverseStringArray.unshift(char)
+        reverseStringArray.unshift(char);
     }
-    console.log(reverseStringArray.join(''))
+    console.log(reverseStringArray.join(''));
 }
 
 // reverseString('Dylan')
 
-function longestSringInArray(arrayOfStrings) {
-    maxLenghthOfString = 0
+// Question 08
+
+const longestSringInArray = function (arrayOfStrings) {
+    maxLenghthOfString = 0;
     for (let string of arrayOfStrings) {
-        if (string.length > maxLenghthOfString) maxLenghthOfString = string.length
+        if (string.length > maxLenghthOfString) maxLenghthOfString = string.length;
     }
-    console.log(maxLenghthOfString)
+    console.log(maxLenghthOfString);
 }
 
-// longestSringInArray(['cat', 'runner', 'encyclopedia'])
+// longestSringInArray(['cat', 'runner', 'ti'])
+
+// Question 09
 
 function stringsLongerThan(array_Strings, len) {
-    outPutArray = []
+    outPutArray = [];
     for (let string of array_Strings) {
-        if (string.length > len) outPutArray.push(string)
+        if (string.length > len) outPutArray.push(string);
     }
     console.log(outPutArray)
 }
